6ca811b6c60dea7f2749aa8ac9d640e3dc64adba
InfratorXD <guiguicazeee@gmail.com>
2023-11-22 15:19:49 -0300

    alterações

--- Projeto.1_2.c
+++ Projeto.1_2.c
@@ -18,6 +18,7 @@ struct cadastro_filmes{
 int main(){
     int senha[4] = {1,2,3,4};
     int senha_informada;
+    int j;
 
     while(1){
         printf("Gerente insira a senha de acesso:\n"); // a senha é 1 2 3 4 (com espaços entre os números)
@@ -47,13 +48,13 @@ int main(){
         //getchar(); 
 
         //Falta arrumar o horário
-        for(int j = 0; j < c[i].num_sessoes; j++){
+        for(j = 0; j < c[i].num_sessoes; j++){
             printf("Qual o horario da sessao %d?\n", j + 1);
             //getchar();
             fgets(c[i].hor_sessoes, sizeof(c[i].hor_sessoes),stdin);
             c[i].hor_sessoes[strcspn(c[i].hor_sessoes, "\n")] = '\0'; // remove a quebra de linha para conseguir inserir um horário no terminal 
             clearBuffer();
-            printf("Quantas cadeiras disponiveis para a sessao das %s\n", c[i].hor_sessoes);
+            printf("Quantas cadeiras disponiveis para a sessao das %d?\n", j);
             scanf("%d", &c[i].cadeiras);
             //getchar();
         }
@@ -67,6 +68,7 @@ int main(){
         printf("\n=============================================\n");
         printf("Filme: %s\n", c[i].filme);
         printf("Quantidade de sessoes: %d\n", c[i].num_sessoes);
+        printf("Horarios das sessoes: %d\n",j);
         for(int j = 0; j < c[i].num_sessoes; j++){
             printf("Sessao %d: %s     Cadeiras disponiveis: %d\n", j + 1, c[i].hor_sessoes, c[i].cadeiras);
         }
