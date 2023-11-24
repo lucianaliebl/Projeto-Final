0dfa954d1f01cc46da1a22591103542416fbe5d1
we-learner <wictormnz@gmail.com>
2023-11-23 23:42:15 -0300

    Melhoria do estilo de codificação

--- Projeto.1_2.c
+++ Projeto.1_2.c
@@ -10,20 +10,21 @@ void clearBuffer()
 
 struct cadastro_filmes
 {
-    char filme[51]; //nome do filme
-    int num_sessoes; // quantas sesões terá o filme
-    char** hor_sessoes; // horário de cada sessão
-    int* cadeiras;  //nº de cadeira para cada sessão
+    char filme[51]; // Nome do filme
+    int num_sessoes; // Quantidade de sessões de cada filme
+    char** hor_sessoes; // Horário de cada sessão
+    int* cadeiras;  // Nº de cadeiras para cada sessão
 };
 
-int main(){
+int main()
+{
     int senha[4] = {1,2,3,4};
     int senha_informada;
     int j;
 
     while(1)
     {
-        printf("Gerente, insira a senha de acesso:\n"); // a senha é 1 2 3 4 (com espaços entre os números)
+        printf("Gerente, insira a senha de acesso:\n"); // A senha é 1 2 3 4 (com espaços entre os números)
         scanf("%d",&senha_informada);
 
         if(senha_informada == senha[0])
@@ -38,15 +39,15 @@ int main(){
         }   
     }
 
-    // Realiza o cadastro dos 4 filmes com quantas sessões disponíveis para cada filme, o horário de cada sessão e o nº de cadeiras em cada sessão
-    struct cadastro_filmes c[4]; // permite o cadastro de 4 filmes 
+    // Realiza o cadastro dos 4 filmes, determinando para cada um deles seus horários, quantidades de sessões e nº de cadeiras em cada sessão
+    struct cadastro_filmes c[4]; // Permite o cadastro de 4 filmes 
     
     for(int i = 0; i < 4; i++)
     {
         clearBuffer();
         printf("Qual o nome do filme a ser cadastrado?\n");
         fgets(c[i].filme, sizeof(c[i].filme), stdin);
-        c[i].filme[strcspn(c[i].filme, "\n")] = '\0'; // remove a quebra de linha para conseguir inserir quantas sessões disponíveis para o filme
+        c[i].filme[strcspn(c[i].filme, "\n")] = '\0'; // Remove a quebra de linha para conseguir inserir quantas sessões disponíveis para o filme
 
         printf("Quantas sessoes disponiveis para o filme %s?\n", c[i].filme);
         scanf("%d", &c[i].num_sessoes);
@@ -58,7 +59,7 @@ int main(){
         for(j = 0; j < c[i].num_sessoes; j++)
         {
             printf("Qual o horario da sessao %d?\n", j + 1);
-            //alocação dinamica
+            // Alocação dinâmica
                 c[i].hor_sessoes[j] = (char*)malloc(10 * sizeof(char));
                     scanf("%9s", c[i].hor_sessoes[j]); 
                         clearBuffer();
